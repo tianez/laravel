2e25a35d8e4b35d065a17cdb9d30c3b7230fa7f8
tianez <saber_tz@163.com>
2016-08-24 17:38:24 +0800

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,52 +8,71 @@ use DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
-
-	public function __construct() {
-		// $this->middleware('auth',['except' => 'login']);
-	}
-
-	public function index(Request $request) {
-		return view('home');
-	}
-
-	public function index_post(Request $request) {
-		$name = $request->id;
-		$password = $request->password;
-		$user = DB::connection('tj')->select('SELECT * FROM day_members WHERE day_members.client_number = '.$name.' AND day_members.`password` = '.$password);
-		if($user){
-			dump($user);
-			echo count($user);
-			cookie('cur_user', $user[0]);
-			return redirect('result');
-		}else {
-			// $request->session()->put('msg', 'value2');
-			$request->session()->flash('msg', 'Task was successful!');
-			// session(['msg' => 'value']);
-			return redirect('/');
-		}
-	}
-
-	public function result(Request $request) {
-		return view('login');
-	}
-
+    
+    public function __construct() {
+        // $this->middleware('auth',['except' => 'login']);
+    }
+    
+    public function index(Request $request) {
+        $user = session('cur_user');
+        dump($user);
+        if($user){
+            $results = DB::table('report')->where('client_number', $user->client_number)->get();
+            $res = array();
+            foreach ($results as $result) {
+                if ($result->result == ' 空')
+                    continue;
+                if (empty($res[$result->item])) {
+                    $res[$result->item] = $result->result;
+                } else {
+                    $res[$result->item] .= '<br>' . $result->result;
+                }
+            }
+            $d['res'] = $res;
+            $d['user'] = $user;
+            return view('result',$d);
+        }else{
+            return view('home');
+        }
+    }
+    
+    public function index_post(Request $request) {
+        $name = $request->id;
+        $password = $request->password;
+        if(empty($name) ||empty($password) ){
+            $request->session()->flash('msg', '体检单号或查询密码不能为空！');
+        }
+        $user = DB::table('members')->where('client_number', $name)->where('password', $password)->first();
+        if($user){
+            session(['cur_user' => $user]);
+        }else {
+            // $request->session()->put('msg', 'value2');
+            $request->session()->flash('msg', 'Task was successful!');
+            // session(['msg' => 'value']);
+        }
+        return redirect('/');
+    }
+    
+    public function result(Request $request) {
+        return view('login');
+    }
+    
     public function login(Request $request) {
-		return view('login');
-	}
-
-	public function login_post(request $request) {
-		$req = $request->all();
-		$data = array('user_name' => $req['username'], 'password' => $req['password']);
-		$auth = Auth::attempt($data, true);
-		$res = array();
-		if ($auth) {
-			$res['state'] = 'ok';
-			$res['data'] =  Auth::user();
-		} else {
-			$res['state'] = 'error';
-			$res['msg'] = '用户名或密码错误！';
-		}
-		return response()->json($res);
-	}
+        return view('login');
+    }
+    
+    public function login_post(request $request) {
+        $req = $request->all();
+        $data = array('user_name' => $req['username'], 'password' => $req['password']);
+        $auth = Auth::attempt($data, true);
+        $res = array();
+        if ($auth) {
+            $res['state'] = 'ok';
+            $res['data'] =  Auth::user();
+        } else {
+            $res['state'] = 'error';
+            $res['msg'] = '用户名或密码错误！';
+        }
+        return response()->json($res);
+    }
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,6 +1,4 @@
-@extends('layout.master') 
-@section('title', 'Page Title') 
-@section('content')
+@extends('layout.master') @section('title', 'Page Title') @section('content')
 <form class="form-horizontal" method="post">
   <div class="weui_cells_title">体检查询</div>
   <div class="weui_cells weui_cells_form">
@@ -25,4 +23,38 @@
     <button type="submit" class="weui_btn weui_btn_primary">提交查询</button>
   </div>
 </form>
+<table class="table table-bordered">
+  <tbody>
+    <tr>
+      <td>你的姓名</td>
+      <td>{{$user->client_name}}</td>
+      <td>身份证号</td>
+      <td>{{ (isset($user->china_id) && $user->china_id!=='') ? $user->china_id : 'Default' }}</td>
+    </tr>
+    <tr>
+      <td>体检单号</td>
+      <td>{{$user->client_number}}</td>
+      <td>手机号码</td>
+      <td>{{$user->mobile_phone or 'sdsdsdssdsdsds'}}</td>
+    </tr>
+    <tr>
+      <td>体检日期</td>
+      <td>2015-08-21 14:53:02</td>
+      <td>报告日期</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+<table class="table table-bordered">
+  <tr>
+    <th>体检项目</th>
+    <th>详细报告</th>
+  </tr>
+  @foreach ($res as $key=> $r)
+  <tr>
+    <td>{{$key}}</td>
+    <td>{{$r}}</td>
+  </tr>
+  @endforeach
+</table>
 @endsection
